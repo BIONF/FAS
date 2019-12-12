8ec60f31f4aa0b2c9dcbd8c5d2225655adc88e26
julian <juliandosch@web.de>
2019-12-12 11:39:55 +0100

    working on installable conda version

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
-set -e
+# #!/bin/bash
+# set -e
 
-# $PYTHON setup.py install     # Python command to install the script.
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt    # Python command to install the script.
 
-BINARY_HOME=$PREFIX/bin
-TESTPKG_HOME=$PREFIX/$PKG_NAME-$PKG_VERSION
-
-# Copy source to the conda environment
-mkdir -p $TESTPKG_HOME
-cp -R $SRC_DIR/* $TESTPKG_HOME/
-
-echo "INSTALL FAS..."
-$PYTHON $TESTPKG_HOME/bin/setup.py install --single-version-externally-managed --record=record.txt
+#BINARY_HOME=$PREFIX/bin
+#TESTPKG_HOME=$PREFIX/$PKG_NAME-$PKG_VERSION
+#
+## Copy source to the conda environment
+#mkdir -p $TESTPKG_HOME
+#cp -R $SRC_DIR/* $TESTPKG_HOME/
+#
+#echo "INSTALL FAS..."
+#$PYTHON $TESTPKG_HOME/bin/setup.py install --single-version-externally-managed --record=record.txt
