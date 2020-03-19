8ef4d470092b657e7c180c5ed7450c90288a3423
julian <juliandosch@web.de>
2020-03-19 10:34:10 +0100

    finished the first version of the flps annotation module

--- greedyFAS/annoFLPS.py
+++ greedyFAS/annoFLPS.py
@@ -25,11 +25,37 @@ import subprocess
 from sys import argv
 
 
-def main(fastapath, outpath, flpspath, threshold):
+def main(fastapath, outpath, flpspath, threshold, prot_lengths):
     flps_out = subprocess.run([flpspath + ' ' + fastapath + ' -s -t ' + threshold], shell=True, capture_output=True)
-    print(flps_out.stdout.decode().split('\n'))
-    ### to be added: parse stdout, somehow get all prot ids from sequence files stop proteins from disappearing
+    lines = flps_out.stdout.decode().split('\n')
+    proteome = {}
+    for i in prot_lengths:
+        proteome[i] = {}
+    for line in lines:
+        cells = line.split('\t')
+        feature = cells[1] + '_' + cells[7]
+        if feature in proteome[cells[0]]:
+            proteome[cells[0]][feature].append((cells[3], cells[4]))
+        else:
+            proteome[cells[0]][feature] = [(cells[3], cells[4])]
+    write_xml(outpath, proteome, prot_lengths)
+    ### to be added: somehow get all prot ids from sequence files to stop proteins from disappearing (should be in the main anno script)
+
+
+def write_xml(outpath, proteome, prot_lengths):
+    with open(outpath, 'w') as out:
+        out.write('<?xml version="1.0"?>\n<tool name="fLPS">\n')
+        for protein in proteome:
+            out.write('\t<protein id="' + protein + '" length="' + str(prot_lengths[protein]) + '">\n')
+            for feature in proteome[protein]:
+                out.write('\t\t<feature type="' + feature + '" instance="' + str(len(proteome[protein][feature])) +
+                          '">\n')
+                for instance in proteome[protein][feature]:
+                    out.write('\t\t\t<start start="' + instance[0] + '">\n\t\t\t<end end="' + instance[1] + '">\n')
+                out.write('\t\t</feature>\n')
+            out.write('\t</protein>\n')
+        out.write('</tool>')
 
 
 if __name__ == '__main__':
-    main(argv[1], argv[2], argv[3], argv[4])
+    main(argv[1], argv[2], argv[3], argv[4], argv[5])
